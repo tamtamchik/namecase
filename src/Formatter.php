6a7787708810daea6e9b55f236330f087f4d4b1c
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2016-01-29 10:52:45 +0300

    small refactoring

--- src/Formatter.php
+++ src/Formatter.php
@@ -21,52 +21,89 @@ class Formatter
     {
         $options = array_merge(self::$options, $options);
 
-        //Skip if string is mixed case
+        // Do not do anything if string is mixed and lazy option is true.
         if ($options['lazy']) {
-            $firstLetterLower = $string[0] == mb_strtolower($string[0]);
-            $allLowerOrUpper  = (mb_strtolower($string) == $string || mb_strtoupper($string) == $string);
-
-            if ( ! ($firstLetterLower || $allLowerOrUpper)) return $string;
+            if (self::skipMixed($string)) return $string;
         }
 
         $local = mb_strtolower($string);
 
         // Capitalize
-        $local = mb_ereg_replace_callback('\b\w', function ($matches) {
-            return mb_strtoupper($matches[0]);
-        }, $local);
-
-        $local = mb_ereg_replace_callback('\'\w\b', function ($matches) {
-            return mb_strtolower($matches[0]);
-        }, $local); # Lowercase 's
+        $local = self::capitalize($local);
 
         if ($options['irish']) {
-
-            if (mb_ereg_match('\bMac[A-Za-z]{2,}[^aciozj]\b', $local) || mb_ereg_match('\bMc', $local)) {
-                $local = mb_ereg_replace_callback('\b(Ma?c)([A-Za-z]+)', function ($matches) {
-                    return $matches[1]
-                    . mb_strtoupper(mb_substr($matches[2], 0, 1))
-                    . mb_substr($matches[2], 1);
-                }, $local);
-
-                // Now fix "Mac" exceptions
-                $local = mb_ereg_replace('\bMacEdo', 'Macedo', $local);
-                $local = mb_ereg_replace('\bMacEvicius', 'Macevicius', $local);
-                $local = mb_ereg_replace('\bMacHado', 'Machado', $local);
-                $local = mb_ereg_replace('\bMacHar', 'Machar', $local);
-                $local = mb_ereg_replace('\bMacHin', 'Machin', $local);
-                $local = mb_ereg_replace('\bMacHlin', 'Machlin', $local);
-                $local = mb_ereg_replace('\bMacIas', 'Macias', $local);
-                $local = mb_ereg_replace('\bMacIulis', 'Maciulis', $local);
-                $local = mb_ereg_replace('\bMacKie', 'Mackie', $local);
-                $local = mb_ereg_replace('\bMacKle', 'Mackle', $local);
-                $local = mb_ereg_replace('\bMacKlin', 'Macklin', $local);
-                $local = mb_ereg_replace('\bMacKmin', 'Mackmin', $local);
-                $local = mb_ereg_replace('\bMacQuarie', 'Macquarie', $local);
-            }
-            $local = mb_ereg_replace('Macmurdo', 'MacMurdo', $local);
+            $local = self::fixIrish($local);
         }
 
         return $local;
+    }
+
+    /**
+     * Skip if string is mixed case.
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+    private static function skipMixed($string)
+    {
+        $firstLetterLower = $string[0] == mb_strtolower($string[0]);
+        $allLowerOrUpper  = (mb_strtolower($string) == $string || mb_strtoupper($string) == $string);
+
+        return ! ($firstLetterLower || $allLowerOrUpper);
+    }
+
+    /**
+     * Capitalize first letters.
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    private static function capitalize($string)
+    {
+        $string = mb_ereg_replace_callback('\b\w', function ($matches) {
+            return mb_strtoupper($matches[0]);
+        }, $string);
+
+        // Lowercase 's
+        $string = mb_ereg_replace_callback('\'\w\b', function ($matches) {
+            return mb_strtolower($matches[0]);
+        }, $string);
+
+        return $string;
+    }
+
+    /**
+     * Fix Irish names.
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    private static function fixIrish($string)
+    {
+        if (mb_ereg_match('\bMac[A-Za-z]{2,}[^aciozj]\b', $string) || mb_ereg_match('\bMc', $string)) {
+            $string = mb_ereg_replace_callback('\b(Ma?c)([A-Za-z]+)', function ($matches) {
+                return $matches[1] . mb_strtoupper(mb_substr($matches[2], 0, 1)) . mb_substr($matches[2], 1);
+            }, $string);
+
+            // Now fix "Mac" exceptions
+            $string = mb_ereg_replace('\bMacEdo', 'Macedo', $string);
+            $string = mb_ereg_replace('\bMacEvicius', 'Macevicius', $string);
+            $string = mb_ereg_replace('\bMacHado', 'Machado', $string);
+            $string = mb_ereg_replace('\bMacHar', 'Machar', $string);
+            $string = mb_ereg_replace('\bMacHin', 'Machin', $string);
+            $string = mb_ereg_replace('\bMacHlin', 'Machlin', $string);
+            $string = mb_ereg_replace('\bMacIas', 'Macias', $string);
+            $string = mb_ereg_replace('\bMacIulis', 'Maciulis', $string);
+            $string = mb_ereg_replace('\bMacKie', 'Mackie', $string);
+            $string = mb_ereg_replace('\bMacKle', 'Mackle', $string);
+            $string = mb_ereg_replace('\bMacKlin', 'Macklin', $string);
+            $string = mb_ereg_replace('\bMacKmin', 'Mackmin', $string);
+            $string = mb_ereg_replace('\bMacQuarie', 'Macquarie', $string);
+        }
+
+        return mb_ereg_replace('Macmurdo', 'MacMurdo', $string);
     }
 }
