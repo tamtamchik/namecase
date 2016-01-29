aba0deb48d2716f2dd47b6b53a039e875dc87220
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2016-01-29 11:24:37 +0300

    force UTF-8 usage

--- src/Formatter.php
+++ src/Formatter.php
@@ -21,12 +21,6 @@ class Formatter
      */
     public static function nc($string, array $options = [])
     {
-        $encoding = mb_internal_encoding();
-
-        var_dump($encoding);
-
-        mb_internal_encoding(self::$encoding);
-
         $options = array_merge(self::$options, $options);
 
         // Do not do anything if string is mixed and lazy option is true.
@@ -34,7 +28,7 @@ class Formatter
             if (self::skipMixed($string)) return $string;
         }
 
-        $local = mb_strtolower($string);
+        $local = mb_strtolower($string, self::$encoding);
 
         // Capitalize
         $local = self::capitalize($local);
@@ -42,8 +36,6 @@ class Formatter
         if ($options['irish']) {
             $local = self::fixIrish($local);
         }
-
-        mb_internal_encoding($encoding);
 
         return $local;
     }
@@ -57,8 +49,8 @@ class Formatter
      */
     private static function skipMixed($string)
     {
-        $firstLetterLower = $string[0] == mb_strtolower($string[0]);
-        $allLowerOrUpper  = (mb_strtolower($string) == $string || mb_strtoupper($string) == $string);
+        $firstLetterLower = $string[0] == mb_strtolower($string[0], self::$encoding);
+        $allLowerOrUpper  = (mb_strtolower($string, self::$encoding) == $string || mb_strtoupper($string, self::$encoding) == $string);
 
         return ! ($firstLetterLower || $allLowerOrUpper);
     }
@@ -73,12 +65,12 @@ class Formatter
     private static function capitalize($string)
     {
         $string = \mb_ereg_replace_callback('\b\w', function ($matches) {
-            return mb_strtoupper($matches[0]);
+            return mb_strtoupper($matches[0], self::$encoding);
         }, $string);
 
         // Lowercase 's
         $string = \mb_ereg_replace_callback('\'\w\b', function ($matches) {
-            return mb_strtolower($matches[0]);
+            return mb_strtolower($matches[0], self::$encoding);
         }, $string);
 
         return $string;
@@ -94,24 +86,33 @@ class Formatter
     private static function fixIrish($string)
     {
         if (mb_ereg_match('\bMac[A-Za-z]{2,}[^aciozj]\b', $string) || mb_ereg_match('\bMc', $string)) {
+
             $string = \mb_ereg_replace_callback('\b(Ma?c)([A-Za-z]+)', function ($matches) {
-                return $matches[1] . mb_strtoupper(mb_substr($matches[2], 0, 1)) . mb_substr($matches[2], 1);
+                return $matches[1]
+                . mb_strtoupper(mb_substr($matches[2], 0, 1, self::$encoding), self::$encoding)
+                . mb_substr($matches[2], 1, null, self::$encoding);
             }, $string);
 
+            $exceptions = [
+                '\bMacEdo'     => 'Macedo',
+                '\bMacEvicius' => 'Macevicius',
+                '\bMacHado'    => 'Machado',
+                '\bMacHar'     => 'Machar',
+                '\bMacHin'     => 'Machin',
+                '\bMacHlin'    => 'Machlin',
+                '\bMacIas'     => 'Macias',
+                '\bMacIulis'   => 'Maciulis',
+                '\bMacKie'     => 'Mackie',
+                '\bMacKle'     => 'Mackle',
+                '\bMacKlin'    => 'Macklin',
+                '\bMacKmin'    => 'Mackmin',
+                '\bMacQuarie'  => 'Macquarie',
+            ];
+
             // Now fix "Mac" exceptions
-            $string = mb_ereg_replace('\bMacEdo', 'Macedo', $string);
-            $string = mb_ereg_replace('\bMacEvicius', 'Macevicius', $string);
-            $string = mb_ereg_replace('\bMacHado', 'Machado', $string);
-            $string = mb_ereg_replace('\bMacHar', 'Machar', $string);
-            $string = mb_ereg_replace('\bMacHin', 'Machin', $string);
-            $string = mb_ereg_replace('\bMacHlin', 'Machlin', $string);
-            $string = mb_ereg_replace('\bMacIas', 'Macias', $string);
-            $string = mb_ereg_replace('\bMacIulis', 'Maciulis', $string);
-            $string = mb_ereg_replace('\bMacKie', 'Mackie', $string);
-            $string = mb_ereg_replace('\bMacKle', 'Mackle', $string);
-            $string = mb_ereg_replace('\bMacKlin', 'Macklin', $string);
-            $string = mb_ereg_replace('\bMacKmin', 'Mackmin', $string);
-            $string = mb_ereg_replace('\bMacQuarie', 'Macquarie', $string);
+            foreach ($exceptions as $pattern => $replacement) {
+                $string = mb_ereg_replace($pattern, $replacement, $string);
+            }
         }
 
         return mb_ereg_replace('Macmurdo', 'MacMurdo', $string);
