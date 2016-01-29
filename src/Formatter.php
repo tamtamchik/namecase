888771a37efc6e7e2dac5ffc561903c02acdd12e
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2016-01-29 14:20:51 +0300

    refactoring

--- src/Formatter.php
+++ src/Formatter.php
@@ -11,7 +11,21 @@ class Formatter
         'spanish' => true,
     ];
 
-    private static $encoding = 'UTF-8';
+    private static $exceptionsIrish = [
+        '\bMacEdo'     => 'Macedo',
+        '\bMacEvicius' => 'Macevicius',
+        '\bMacHado'    => 'Machado',
+        '\bMacHar'     => 'Machar',
+        '\bMacHin'     => 'Machin',
+        '\bMacHlin'    => 'Machlin',
+        '\bMacIas'     => 'Macias',
+        '\bMacIulis'   => 'Maciulis',
+        '\bMacKie'     => 'Mackie',
+        '\bMacKle'     => 'Mackle',
+        '\bMacKlin'    => 'Macklin',
+        '\bMacKmin'    => 'Mackmin',
+        '\bMacQuarie'  => 'Macquarie',
+    ];
 
     /**
      * @param string $string
@@ -29,7 +43,7 @@ class Formatter
         }
 
         // Capitalize
-        $local = mb_convert_case(mb_strtolower($string, self::$encoding), MB_CASE_TITLE, self::$encoding);
+        $local = mb_convert_case(mb_strtolower($string), MB_CASE_TITLE);
 
         if ($options['irish']) {
             $local = self::updateIrish($local);
@@ -47,8 +61,8 @@ class Formatter
      */
     private static function skipMixed($string)
     {
-        $firstLetterLower = $string[0] == mb_strtolower($string[0], self::$encoding);
-        $allLowerOrUpper  = (mb_strtolower($string, self::$encoding) == $string || mb_strtoupper($string, self::$encoding) == $string);
+        $firstLetterLower = $string[0] == mb_strtolower($string[0]);
+        $allLowerOrUpper  = (mb_strtolower($string) == $string || mb_strtoupper($string) == $string);
 
         return ! ($firstLetterLower || $allLowerOrUpper);
     }
@@ -65,29 +79,11 @@ class Formatter
         if (mb_ereg_match('\bMac[A-Za-z]{2,}[^aciozj]\b', $string) || mb_ereg_match('\bMc', $string)) {
 
             $string = \mb_ereg_replace_callback('\b(Ma?c)([A-Za-z]+)', function ($matches) {
-                return $matches[1]
-                . mb_strtoupper(mb_substr($matches[2], 0, 1, self::$encoding), self::$encoding)
-                . mb_substr($matches[2], 1, null, self::$encoding);
+                return $matches[1] . mb_strtoupper(mb_substr($matches[2], 0, 1)) . mb_substr($matches[2], 1);
             }, $string);
 
-            $exceptions = [
-                '\bMacEdo'     => 'Macedo',
-                '\bMacEvicius' => 'Macevicius',
-                '\bMacHado'    => 'Machado',
-                '\bMacHar'     => 'Machar',
-                '\bMacHin'     => 'Machin',
-                '\bMacHlin'    => 'Machlin',
-                '\bMacIas'     => 'Macias',
-                '\bMacIulis'   => 'Maciulis',
-                '\bMacKie'     => 'Mackie',
-                '\bMacKle'     => 'Mackle',
-                '\bMacKlin'    => 'Macklin',
-                '\bMacKmin'    => 'Mackmin',
-                '\bMacQuarie'  => 'Macquarie',
-            ];
-
             // Now fix "Mac" exceptions
-            foreach ($exceptions as $pattern => $replacement) {
+            foreach (self::$exceptionsIrish as $pattern => $replacement) {
                 $string = mb_ereg_replace($pattern, $replacement, $string);
             }
         }
